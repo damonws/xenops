de56d78ac0b7109349826676fff4d7acec9172b5
Damon <Damon@Damons-MacBook-Air.local>
2015-04-24 00:15:50 +0100

    Initial commit.

---
+++ reboot.py
@@ -0,0 +1,110 @@
+import capstone
+import binascii
+
+def analyze_file(binary):
+    with open(binary) as f:
+        data = f.read()
+    analyze_buffer(data)
+
+def print_detail(insn):
+    if insn is None:
+        return
+
+    print '{:08x}: {}  {:8}{}'.format(insn.address,
+                                      binascii.hexlify(insn.bytes),
+                                      insn.mnemonic, insn.op_str)
+
+    if insn.id == capstone.arm.ARM_INS_INVALID:
+        return
+
+    if len(insn.regs_read) > 0:
+        print "\tImplicit registers read: ",
+        for m in insn.regs_read:
+            print "%s " % insn.reg_name(m),
+        print
+
+    if len(insn.regs_write) > 0:
+        print "\tImplicit registers modified: ",
+        for m in insn.regs_write:
+            print "%s " % insn.reg_name(m),
+        print
+
+    if len(insn.groups) > 0:
+        print "\tThis instruction belongs to groups: ",
+        for m in insn.groups:
+            print "%s " % insn.group_name(m),
+        print
+
+    if len(insn.operands) > 0:
+        print "\top_count: %u" % len(insn.operands)
+        c = 0
+        for i in insn.operands:
+            if i.type == capstone.arm.ARM_OP_REG:
+                print "\t\toperands[%u].type: REG = %s" % (c, insn.reg_name(i.reg))
+            if i.type == capstone.arm.ARM_OP_IMM:
+                print "\t\toperands[%u].type: IMM = 0x%x" % (c, i.imm & 0xffffffff)
+            if i.type == capstone.arm.ARM_OP_PIMM:
+                print "\t\toperands[%u].type: P-IMM = %u" % (c, i.imm)
+            if i.type == capstone.arm.ARM_OP_CIMM:
+                print "\t\toperands[%u].type: C-IMM = %u" % (c, i.imm)
+            if i.type == capstone.arm.ARM_OP_FP:
+                print "\t\toperands[%u].type: FP = %f" % (c, i.fp)
+            if i.type == capstone.arm.ARM_OP_SYSREG:
+                print "\t\toperands[%u].type: SYSREG = %u" % (c, i.reg)
+            if i.type == capstone.arm.ARM_OP_SETEND:
+                if i.setend == capstone.arm.ARM_SETEND_BE:
+                    print "\t\toperands[%u].type: SETEND = be" % c
+                else:
+                    print "\t\toperands[%u].type: SETEND = le" % c
+            if i.type == capstone.arm.ARM_OP_MEM:
+                print "\t\toperands[%u].type: MEM" % c
+                if i.mem.base != 0:
+                    print "\t\t\toperands[%u].mem.base: REG = %s" \
+                        % (c, insn.reg_name(i.mem.base))
+                if i.mem.index != 0:
+                    print "\t\t\toperands[%u].mem.index: REG = %s" \
+                        % (c, insn.reg_name(i.mem.index))
+                if i.mem.scale != 1:
+                    print "\t\t\toperands[%u].mem.scale: %u" \
+                        % (c, i.mem.scale)
+                if i.mem.disp != 0:
+                    print "\t\t\toperands[%u].mem.disp: 0x%x" \
+                        % (c, i.mem.disp & 0xffffffff)
+
+            if i.shift.type != capstone.arm.ARM_SFT_INVALID and i.shift.value:
+                print "\t\t\tShift: %u = %u" \
+                    % (i.shift.type, i.shift.value)
+            if i.vector_index != -1:
+                print "\t\t\toperands[%u].vector_index = %u" %(c, i.vector_index)
+            if i.subtracted:
+                print "\t\t\toperands[%u].subtracted = True" %c
+
+            c += 1
+
+    if insn.update_flags:
+        print "\tUpdate-flags: True"
+    if insn.writeback:
+        print "\tWrite-back: True"
+    if not insn.cc in [capstone.arm.ARM_CC_AL, capstone.arm.ARM_CC_INVALID]:
+        print "\tCode condition: %u" % insn.cc
+    if insn.cps_mode:
+        print "\tCPSI-mode: %u" %(insn.cps_mode)
+    if insn.cps_flag:
+        print "\tCPSI-flag: %u" %(insn.cps_flag)
+    if insn.vector_data:
+        print "\tVector-data: %u" %(insn.vector_data)
+    if insn.vector_size:
+        print "\tVector-size: %u" %(insn.vector_size)
+    if insn.usermode:
+        print "\tUser-mode: True"
+    if insn.mem_barrier:
+        print "\tMemory-barrier: %u" %(insn.mem_barrier)
+    print
+
+def analyze_buffer(data):
+    md = capstone.Cs(capstone.CS_ARCH_ARM, capstone.CS_MODE_ARM)
+    md.skipdata = True
+    md.detail = True
+
+    for insn in md.disasm(data, 0x9ff00000):
+        print_detail(insn)
---
+++ recursive_descent.py
@@ -0,0 +1,121 @@
+import capstone
+import reboot
+import struct
+
+def rd_disasm(data, start=0, entries=None):
+    if entries is None:
+        entries = [start]
+
+    md = capstone.Cs(capstone.CS_ARCH_ARM, capstone.CS_MODE_ARM)
+    md.detail = True
+
+    listing = {}
+    regs = {}
+    max_insn_size = 4
+
+    def opval(insn, i):
+        op = insn.operands[i]
+
+        # register
+        if op.type == capstone.arm.ARM_OP_REG:
+            if op.reg == capstone.arm.ARM_REG_PC:
+                return insn.address + 8
+            else:
+                return regs[op.reg]
+
+        # immediate
+        elif op.type == capstone.arm.ARM_OP_IMM:
+            return op.imm & 0xffffffff
+
+        # memory
+        elif op.type == capstone.arm.ARM_OP_MEM:
+            if op.mem.index != 0 or op.mem.scale != 1:
+                raise KeyError
+            if op.mem.base == capstone.arm.ARM_REG_PC:
+                base = insn.address + 8
+            else:
+                base = regs[op.reg]
+            mem = base + op.mem.disp - start
+            if mem + 4 > len(data):
+                raise KeyError
+            return struct.unpack('<I', data[mem:mem+4])[0]
+
+        # other
+        else:
+            return None
+
+    def disasm(addr):
+        new_entries = []
+        if addr not in listing:
+            if addr % 2:
+                md.mode = capstone.CS_MODE_THUMB
+            else:
+                md.mode = capstone.CS_MODE_ARM
+
+            insn = next(md.disasm(data[addr-start:addr-start+max_insn_size],
+                                  addr, 1), None)
+
+            listing[addr] = {
+                'insn' : insn,
+            }
+
+            if insn:
+                if addr in regs:
+                    new_regs = regs[addr]
+                    for reg, value in regs[addr].iteritems():
+                        print '{}={:x}'.format(insn.reg_name(reg), value)
+                else:
+                    new_regs = {}
+                reboot.print_detail(insn)
+
+                # analyze semantics of instructions
+                # - determine possible next instructions
+                # - set register state
+                if capstone.CS_GRP_JUMP in insn.groups:
+
+                    # add target of jump instructions
+                    if insn.operands[0].type == capstone.arm.ARM_OP_IMM:
+                        new_entries.append(insn.operands[0].imm & 0xffffffff)
+                    else:
+                        # TODO: handle jumps to registers (e.g. bx r1)
+                        pass
+
+                    # add next instruction for conditional jumps
+                    if insn.cc != capstone.arm.ARM_CC_AL:
+                        new_entries.append(addr + insn.size)
+
+                else:
+                    # add next instruction for everything else
+                    new_entries.append(addr + insn.size)
+
+                if insn.id == capstone.arm.ARM_INS_ADD:
+                    try:
+                        new_regs[insn.operands[0].reg] = opval(insn, 1) + opval(insn, 2)
+                    except KeyError:
+                        print '>>> ADD key error <<<'
+                elif insn.id == capstone.arm.ARM_INS_SUB:
+                    try:
+                        new_regs[insn.operands[0].reg] = opval(insn, 1) - opval(insn, 2)
+                    except KeyError:
+                        print '>>> SUB key error <<<'
+                elif insn.id == capstone.arm.ARM_INS_LDR:
+                    try:
+                        new_regs[insn.operands[0].reg] = opval(insn, 1)
+                    except KeyError:
+                        print '>>> LDR key error <<<'
+
+                if new_regs:
+                    for entry in new_entries:
+                        regs.setdefault(entry, {}).update(new_regs)
+
+        return new_entries
+
+    while entries:
+        #print ','.join(hex(e) for e in entries)
+        entries.extend(disasm(entries.pop()))
+
+if __name__ == '__main__':
+    import sys
+    for filename in sys.argv[1:]:
+        with open(filename) as f:
+            rd_disasm(f.read(), 0x9ff00000)
